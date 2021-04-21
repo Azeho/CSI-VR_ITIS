c278b4b98800b2af1aa3b6ad6250b892f62cda75
Begdan <beg.beg42@gmail.com>
2021-04-21 17:38:56 +0300

    idk

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -6,20 +6,12 @@ using UnityEngine.Experimental.GlobalIllumination;
 public class Flashlight : MonoBehaviour
 {
     [SerializeField] private GameObject light;
-    [SerializeField] private GameObject objectsToReveal;
     
     public void TurnOnOff()
     {
         if (!light.activeSelf)
-        {
             light.SetActive(true);
-            objectsToReveal.SetActive(true);
-        }
-        else
-        {
-            light.SetActive(false);
-            objectsToReveal.SetActive(false);
-        }
+        else light.SetActive(false);
     }
     
 }
