ebff7078fb47400b2aa06c1d4bb99edd1af9473b
Begdan <beg.beg42@gmail.com>
2021-05-26 09:38:16 +0300

    added scripts for changing the player's name and UI

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public static string PlayerName;
+    private StringBuilder playerNameBuilder;
+    [SerializeField] private TextMeshProUGUI displayName;
+    [SerializeField] private TextMeshProUGUI UIName;
+    [SerializeField] private GameObject maxCharactersCountError;
+    [SerializeField] private GameObject enterNameError;
+    [SerializeField] private GameObject playerUI;
+    [SerializeField] private GameObject VRRig;
+    [SerializeField] private GameObject menuUI;
+    
     void Update()
     {
-        
+        PlayerName = playerNameBuilder.ToString();
+        displayName.text = PlayerName;
+        UIName.text = PlayerName;
+    }
+
+    public void ChangeName(TextMeshProUGUI inputButton)
+    {
+        if (playerNameBuilder.Length <= 16)
+            playerNameBuilder.Append(inputButton.text);
+        else
+        {
+            enterNameError.SetActive(false);
+            maxCharactersCountError.SetActive(true);
+        }
+            
+    }
+
+    public void StartGame()
+    {
+        if (playerNameBuilder.Length > 0)
+        {
+            menuUI.SetActive(false);
+            playerUI.SetActive(true);
+            VRRig.gameObject.transform.position = new Vector3(2, 0, -9);
+            VRRig.gameObject.transform.Rotate(0f, 200f, 0f, Space.World);
+        }
+        else
+        {
+            maxCharactersCountError.SetActive(false);
+            enterNameError.SetActive(true);
+        }
+            
     }
 }
