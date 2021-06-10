90bf3cb20d489b37ba3ef57f47d1d68657887d2f
Begdan <beg.beg42@gmail.com>
2021-06-10 17:23:51 +0300

    added button to display images

--- Assets/Scripts/PhotoController.cs
+++ Assets/Scripts/PhotoController.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class PhotoController : MonoBehaviour
 {
-    void Start()
+    private Object[] photos;
+    [SerializeField] private GameObject photoPrefab;
+    [SerializeField] private GameObject photoSpawner;
+    [SerializeField] private GameObject pinBoard;
+    [SerializeField] private float offsetZ = 1f;
+    [SerializeField] private float offsetY;
+    private Vector3 parentTransform;
+
+    private void Awake()
     {
-        
+        parentTransform = photoSpawner.transform.position;
     }
-    
-    void Update()
+
+    public void LoadPhotos()
     {
-        Object[] photos = Resources.LoadAll("", typeof(Texture2D));
+        photos = Resources.LoadAll("", typeof(Texture2D));
+        pinBoard.SetActive(true);
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject spawnedPhoto = Instantiate(photoPrefab, new Vector3(parentTransform.x, parentTransform.y,
+                parentTransform.z - i), Quaternion.identity);
+            Material mat = new Material(Shader.Find("Standard"));
+        }
     }
 }
